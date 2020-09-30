7daf702f1f6a6977ade971b155ee75aa2dad23a7
Amar Maksumić <amarmaksumich@gmail.com>
2020-09-29 23:28:41 -0400

    added fading navbar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 jQuery(document).ready(function( $ ) {
 
+	var mywindow = $(window);
+	var mypos = mywindow.scrollTop();
+	var up = false;
+	var newscroll;
+	mywindow.scroll(function () {
+			newscroll = mywindow.scrollTop();
+			if (newscroll > mypos && !up) {
+					$('.navbar').stop().fadeOut();
+					up = !up;
+					console.log(up);
+			} else if(newscroll < mypos && up) {
+					$('.navbar').stop().fadeIn();
+					up = !up;
+			}
+			mypos = newscroll;
+	});
+
 	if($('#canvas').length) {
 
 		var doughnutData = [{
